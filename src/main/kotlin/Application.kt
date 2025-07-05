b213e8b81b6141d577052870e487ec82f512f3b8
pin-nattharin <nattharin3004@gmail.com>
2025-07-05 15:52:55 +0700

    update

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,21 @@
+package com.nattharin
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    configureRouting()
+
+}
